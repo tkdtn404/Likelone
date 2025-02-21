40e78a89d6bf9afab4afa515087fafe5e8d21b28
tkdtn404 <92982995+tkdtn404@users.noreply.github.com>
2025-02-21 10:44:57 +0900

    Likeliin

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age;//정수형 변수
+            age = 25; //변수에 값 시정
+
+            Console.WriteLine(age); // 변수에 저장된 값은  출력 25
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 리더럴 코드에서 고정된 값을 의미한다
+            int number = 10;  //정수형 리터럴
+            double p1 = 1.14; //실수형 리터럴
+            char letter = 'A'; // 문자 리터럴
+            string name = "Alive"; //문자열 리터럴;
+
+            Console.WriteLine(number); // 출력 10
+            Console.WriteLine(p1); // 출력 1.14
+            Console.WriteLine(letter); // 출력 A
+            Console.WriteLine(name); // 출력 Alive
+        }
+    }
+}
