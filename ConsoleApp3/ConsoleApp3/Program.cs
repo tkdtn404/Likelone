15cd3c45743346835eb3f415510a15a3e0461d0f
tkdtn404 <92982995+tkdtn404@users.noreply.github.com>
2025-02-21 10:59:45 +0900

    Likelone1

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //캐릭터
+            int hp = 100;
+            double att = 56.7;
+            string name = "카타리나";
+            char tear = 's';
+
+            Console.WriteLine("HP : " + hp);
+            Console.WriteLine("공격력 : " + att);
+            Console.WriteLine("캐릭터 이름 : " + name);
+            Console.WriteLine("등급 : " + tear);
+        }
+    }
+}
