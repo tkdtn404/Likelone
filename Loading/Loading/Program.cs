2860db8ed1d1f5b0c1a5e83c6e11c3e46b13b4f1
tkdtn404 <92982995+tkdtn404@users.noreply.github.com>
2025-02-21 15:04:27 +0900

    Likelone

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000); // 1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000); // 1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000); // 1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000); // 1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000); // 1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000); // 1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000); // 1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000); // 1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000); // 1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000); // 1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■■");
+            Thread.Sleep(1000); // 1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("텍스트엘든링");
+
+            Console.WriteLine("스토리1");
+
+        }
+    }
+}
