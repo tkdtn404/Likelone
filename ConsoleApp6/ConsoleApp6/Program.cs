81ea85c19442355d81d4d7e27954bb701842a1be
tkdtn404 <92982995+tkdtn404@users.noreply.github.com>
2025-02-21 16:02:27 +0900

    Likelone

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*//사용자 입력을 문자열 받기
+            Console.WriteLine("이름을 입력하세요");
+            string userName = Console.ReadLine(); // 사용자로 부터 입력 받기
+
+            Console.WriteLine("안녕하세요, (userName)님!");*/
+
+            /* //문자열을 정수로 변환
+             Console.Write("나이를 입력하세요 : ");
+             string input = Console.ReadLine(); //사용자로부터 입력 받기
+             int age = int.Parse(input); //문자열을 정수로 변환
+
+             Console.WriteLine("$내년에는 {age + 1} 삶이 되겠군요!"); //변환된 값 사용
+             Console.WriteLine("$내년에는 " + age + "삶이 되겠군요!");
+             Console.WriteLine("내년에는 {0} 삶이 되겠군요!", age);*/
+
+            /*float skil_Damage = 0.0f;
+            float skill_ghl = 0.0f;
+            float skill_ja = 0.0f;
+            int Maxmana = 0;
+            int mana = 0;
+            int culmana = 0;
+            float ei = 0.0f;
+            float gag_Damage = 0.0f;
+            float tal = 0.0f;
+            float un = 0.0f;
+
+
+
+            Console.WriteLine("스킬 피해량을 입력하세요 : ");
+            skil_Damage = float.Parse(Console.ReadLine());
+            Console.WriteLine("카드 게이트 획득량을 입력하세요 : ");
+            skill_ghl = float.Parse(Console.ReadLine());
+            Console.WriteLine("전투 중 마나 회복량을 입력하세요 : ");
+            culmana = int.Parse(Console.ReadLine());
+            Console.WriteLine("비전투 중 마나 회복량을 입력하세요 : ");
+            mana = int.Parse(Console.ReadLine());
+            Console.WriteLine("최대 마나을 입력하세요 : ");
+            Maxmana = int.Parse(Console.ReadLine());
+            Console.WriteLine("각성기 피해량을 입력하세요 : ");
+            gag_Damage = float.Parse(Console.ReadLine());
+            Console.WriteLine("이동 속도을 입력하세요 : ");
+            ei = float.Parse(Console.ReadLine());
+            Console.WriteLine("탈 것 속도을 입력하세요 : ");
+            tal = float.Parse(Console.ReadLine());
+            Console.WriteLine("운반 속도을 입력하세요 : ");
+            un = float.Parse(Console.ReadLine());
+            Console.WriteLine("스킬 재사용 대기시간을 입력하세요 : ");
+            skill_ja = float.Parse(Console.ReadLine());
+           
+            Console.WriteLine("루인 스킬 피해 {0} %", skil_Damage);
+            Console.WriteLine("카드 게이트 획득량 {0} %", skill_ghl);
+            Console.WriteLine("전투 중 마나 회복량 {0} ", culmana);
+            Console.WriteLine("비전투 중 마나 회복량 {0} ", mana);
+            Console.WriteLine("최대 마나 {0} ", Maxmana);
+            Console.WriteLine("각성기 피해량 {0} %", gag_Damage);
+            Console.WriteLine("이동 속도 {0} %", ei);
+            Console.WriteLine("탈 것 속도 {0} %", tal);
+            Console.WriteLine("운반 속도 {0} %", un);
+            Console.WriteLine("스킬 재사용 대기시간 {0} %", skill_ja);*/
+        }
+    }
+}
