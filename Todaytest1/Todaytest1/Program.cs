016c763566eedc2ec1ebe0920401a0effc7fbf67
tkdtn404 <92982995+tkdtn404@users.noreply.github.com>
2025-02-21 16:24:29 +0900

    Likelone

---
+++ Todaytest1/Todaytest1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todaytest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("로딩바 엔터를 치면 스토리 시작합니다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("525년 지구에 천상계와 마계로 나누어져 살고 있었습니다. ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("어느날 마계에서 마왐이 나타나 천상계를 침략하였습니다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("천상계는 마왕을 막기 위해 천사들을 보냈습니다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("천상계와마계의 전쟁을 계속 진행되고 있습니다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("5년동안 전쟁이 계속 이어지고 있는 지금 천상계와 마계의 승부는 어떻게 될까요? ");
+
+        }
+    }
+}
