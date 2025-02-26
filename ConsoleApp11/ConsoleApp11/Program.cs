6ddd4369c00132cf4d22209649f83fb4a6982477
tkdtn404 <92982995+tkdtn404@users.noreply.github.com>
2025-02-26 17:28:29 +0900

    likelion

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* //배열
+             // 0부터 시작한다.
+
+             int[] num = new int[3]; //3개 메모리 만들겠다.
+
+             num[0] = 10;
+             num[1] = 20;
+             num[2] = 30;
+
+             for (int i = 0; i < 3; i++)
+             {
+                 Console.WriteLine(num[i]);
+             }*/
+
+            /* int[] numders = { 1, 2, 3 }; //간단한 선언과 초기화
+             int[] numder2 = new int[3];  //크기만 지정
+             int[] numder3 = new int[]{  1, 2, 3 }; //초기화와 함께 선언
+
+             for(int i = 0; i < 3; i++)
+             {
+                 Console.WriteLine(numder3[i]);
+             }*/
+
+            /*string[] fruits = { "사과", "바나나", "오렌지" };
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(fruits[i]);
+            }*/
+
+            //3명의 
+            //국어, 영어, 수학 점수를 입력받고
+            //총점과 평균을 출력하세요.
+
+            /*int[] iKor = new int[3];
+            int[] iEng = new int[3];
+            int[] iMath = new int[3];
+
+            int[] sum = new int[3];
+            float[] aver = new float[3];
+
+
+            //학생입력받기
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("학생 성적입력하세요 : ");
+                Console.Write("국어 : ");
+                iKor[i] = int.Parse(Console.ReadLine());
+                Console.Write("영어 : ");
+                iEng[i] = int.Parse(Console.ReadLine());
+                Console.Write("수학 : ");
+                iMath[i] = int.Parse(Console.ReadLine());
+
+
+                sum[i] = iKor[i] + iEng[i] + iMath[i];
+
+                aver[i] = (float)sum[i] / 3;*/
+
+            /*//1차원 배열
+            int[] scores = new int[3];
+
+            scores[0] = 90;
+            scores[0] = 85;
+            scores[0] = 88;
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine($"점수{i + 1}:{scores[i]}");
+            }*/
+
+            /*double value = 123.456789;
+            //소수점 자릿수 절정하는 포맷
+            Console.WriteLine(value.ToString("F2"));
+            Console.WriteLine($"소수점둘쨰자리 : {value:F2}");
+            Console.WriteLine(String.Format("수소점 둘째 자리: {0:F2}",value));
+            //소수점 없이 정수 출력
+            Console.WriteLine(value.ToString("F0"));*/
+
+
+            /*ouble value = 123124124.12323;
+
+            Console.WriteLine(value.ToString("N2"));*/
+
+            /* //2차원 배열 선언
+             int[,] matrix = new int[2, 3]
+             {
+                 {1, 2, 3 },{4,5,6}
+             };
+             for(int i = 0; i < 2; i++)
+             {
+
+                 for(int j = 0; j < 3; j++)
+                 {
+                     Console.WriteLine($"{matrix[i, j]}");
+                 }
+                 Console.WriteLine();
+             }*/
+
+            /* int[][] matrix = new int[2][];
+             matrix[0] = new int[3];
+             matrix[1] = new int[3];
+
+             //값 입력
+             matrix[0][0] = 1;
+             matrix[0][1] = 2;
+             matrix[0][2] = 3;
+
+             matrix[1][0] = 4;
+             matrix[1][1] = 5;
+             matrix[1][2] = 6;
+
+             //출력
+             for(int i = 0; i < matrix.Length; i++)
+             {
+                 for (int j=0; j < matrix[i].Length; j++)
+                 {
+                     Console.Write(matrix[i][j] + " ");
+                 }
+                 Console.WriteLine();
+             }*/
+
+            /*  //6.가변배열
+              Console.WriteLine("가변 배열");
+              int[][] jaggedArray = new int[3][];
+
+              jaggedArray[0] = new int[] { 1, 2 };
+              jaggedArray[1] = new int[] { 3, 4, 5 };
+              jaggedArray[2] = new int[] { 6 };
+
+
+              for (int i = 0; i < jaggedArray.Length; i++)
+              {
+                  for (int j = 0; j < jaggedArray[i].Length; j++)
+                  {
+                      Console.Write($"{jaggedArray[i][j]}");
+                  }
+                  Console.WriteLine();
+              }
+
+              Console.WriteLine("var 키워드 사용");
+              var numbers = new[] { 1, 2, 3, 4, 5 };
+              Console.WriteLine($"배열 타입: {numbers.GetType()}");*/
+          
+        }
+    }
+}
---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        /*//메모리 영역
+        //스택 (stack)
+        //힙    (heap)
+        //정석메모리 (static memory)
+        //1단게 함수
+        //반환형 함수(매개변수)
+        //{
+        //}
+
+      static void Loading()
+        {
+            Console.WriteLine("로딩중.");
+            Thread.Sleep(1000);
+            Console.WriteLine("로딩중..");
+            Thread.Sleep(1000);
+            Console.WriteLine("로딩중...");
+            Thread.Sleep(1000);
+
+        }
+
+        //2단계]
+        //입력
+        static void AttackFunction(int Attack)
+        {
+            Console.WriteLine("공격력은 : " + Attack);
+        }
+
+        //3단계
+        //출력
+        static int BsseAttack()
+        {
+            //기본공격력
+            int attack = 10;
+
+            return attack;
+
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }*/
+        static int Add(int a,int b)
+        {  
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+
+            /* Loading();
+
+             Console.WriteLine("게임이 시작됩니다.");
+             Console.ReadLine();*//*
+
+             int Attack = 0;
+             int bAttack = 0;
+             Console.WriteLine("캐릭터의 공격력을 입력 : ");
+             Attack = int.Parse(Console.ReadLine());
+             //기본공격력
+             bAttack = BsseAttack();
+
+             AttackFunction(bAttack+Attack);*/
+
+            /*int result = Add(10, 20);
+            Console.WriteLine($"10 + 20 {result}");*/
+
+            /* string[] fruits = { "사과", "바나나", "체리" };
+
+             //반복문
+             foreach(string fruit in fruits)
+             {
+                 Console.WriteLine(fruit);
+             }*/
+            //Console.WriteLine("=================1번째 문제======================");
+
+            /*int[] num = new int[5];
+            int[] num1 = {10, 20, 30, 40, 50};
+            
+            for(int i=0; i<5;i++)        
+            {
+                num[i] += num1[i];
+                Console.WriteLine(num[i]);
+            }*/
+
+            /*int[] num = new int[5];
+            int max = 0;
+            for(int i=0; i < num.Length; i++)
+            {
+                Console.WriteLine("정수를 입력하세요: ");
+                num[i] = int.Parse(Console.ReadLine());
+                max += num[i];
+
+                Console.WriteLine("모든 수의 합은:" + max);
+            }*/
+
+            /*int[] num = {3, 6 ,9, 12, 15};
+            int max = 0;
+            for(int i = 0; i < num.Length-1; i++)
+            {
+                int a = num[i];
+                int b = num[i + 1];
+                if(a < b)
+                {
+                    max = b;
+                }
+            }
+            Console.WriteLine("최대값은 " + max);*/
+
+            /* int a = 1;
+             for(int i =1;i<=10; i++)
+             {
+
+                 Console.WriteLine(i);
+
+             }*/
+
+            /*int a = 1;
+            while(a < 11)
+            {
+                if(a % 2 == 0)
+                {
+                    Console.WriteLine(a);
+                }
+                a++;*/
+            /*int[] numA = { 1, 2, 3, 4, 5 };
+            foreach(int i in numA)
+            {
+                Console.WriteLine(i);
+            }*/
+            /*int sum = Add(3, 5);
+
+            Console.WriteLine("합은 :" + sum);*/
+            /*string str = "HELLO";
+            Console.WriteLine("문자열 입력: " + str);
+            Console.WriteLine("문자열 길이: " + str.Length);*/
+
+            int num = 0;
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("정수를 입력하세요.");
+                int num1 = int.Parse(Console.ReadLine());
+                if(num < num1)
+                {
+                    num = num1;
+                }
+                Console.WriteLine("가장 큰 수:" + num);
+            }
+
+        }
+
+    }
+
+}
